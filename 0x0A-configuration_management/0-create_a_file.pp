c6a7cdfe9cfeebf223a3558eb13f9de2869fb590
Wangutusi Arshton <wangarshton@gmail.com>
2024-02-24 02:54:33 +0300

    removed extra line

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-#creates a file
+# create a file
+
 file { '/tmp/school':
-	mode 	=>'0744',
-	owner 	=> 'www-data',
-	group 	=> 'www-data',
-	content => 'I love Puppet',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
